77f20dee8b4d6399ddbbf082b4b49c9ccee8e916
volynkina <bzzz18@yandex.ru>
2023-01-15 18:18:42 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int deposit = 15_000;
+        int total = 0;
+        int i = 0;
+        for (; total <= 2_459_000; i++) {
+            total = total + deposit;
+            System.out.println("Месяц " + i + " сумма накоплений равна "+ total);
+        }
     }
 }
