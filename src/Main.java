f363a321f2a30d9670cfb2af42c5d013959d6af6
volynkina <bzzz18@yandex.ru>
2023-01-16 12:08:12 +0500

    task 4+5

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
         int i = 0;
         for (; total <= 2_459_000; i++) {
             total = total + deposit;
-            System.out.println("Месяц " + i + " сумма накоплений равна "+ total);
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total);
         }
         int b = 0;
-        while ( b < 10 ) {
-            b= b + 1 ;
+        while (b < 10) {
+            b = b + 1;
             System.out.print(b + " ");
         }
         System.out.println();
-        for ( int c = 10; c > 0 ; c = c - 1) {
+        for (int c = 10; c > 0; c = c - 1) {
             System.out.print(c + " ");
         }
         System.out.println();
@@ -24,10 +24,28 @@ public class Main {
         int populationBorn = 17;
         while (year < 10) {
             year++;
-            population = population / 1000* (populationBorn-populationDead) + population;
+            population = population / 1000 * (populationBorn - populationDead) + population;
             System.out.println("Год " + year + ", численность населения составляет " + population);
         }
 
+        int deposit1 = 15_000;
+        int month = 0;
+        int result = 0;
+        for (; result < 12_000_000; month++) {
+            result = result + result * 7 / 100 + deposit1;
+            System.out.println("Месяц " + month + " - результат накоплений: " + result);
 
+        }
+        System.out.println();
+        int deposit2 = 15_000;
+        int month2 = 0;
+        int result2 = 0;
+        for (; result2 < 12_000_000; month2 ++) {
+            result2 = result2 + result2 * 7 / 100 + deposit2;
+            if (month2 % 6 == 0) {
+                System.out.println("Месяц " + month2 + " - результат накоплений: " + result2);
+            }
+        }
+        System.out.println( result );
     }
 }
